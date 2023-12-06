3cfdcbf0e0bb6f3889eb065c6551a23a03d5b011
Willsson10 <franciscobgmugnaini@gmail.com>
2023-12-06 08:37:20 -0300

    refazendo

--- bernoulli/bernoulli.m
+++ bernoulli/bernoulli.m
@@ -1,47 +1,63 @@
-% Limpa a tela e o workspace
-clc
 clear
+clc
 
-% Lê os valores do arquivo 'entrada.txt'
-fileID = fopen('entrada.txt', 'r');
-resultados = fscanf(fileID, '%f');
-fclose(fileID);
+% Função para calcular a Função de Massa de Probabilidade (PMF) customizada
+function prob_mass = custom_pmf(probability, values)
+  prob_mass = (1 - probability) * (values == 0) + probability * (values == 1);
+  prob_mass = prob_mass(:);
+end
 
-% Obtém a contagem de ocorrências de cada resultado
-contagem_resultados = hist(resultados, unique(resultados));
+% Função para calcular a Função de Distribuição Cumulativa (CDF) customizada
+function cum_dist = custom_cdf(pmf, values)
+  cum_dist = cumsum(pmf);
+  cum_dist(floor(values) > 1) = 1;
+end
 
-% Gera a PMF e a CDF para a distribuição Bernoulli
-probabilidadeSucesso = input('Digite a probabilidade de sucesso (valores entre 0 e 1): ');
-pmfResultados = calcularBernoulliPMF(probabilidadeSucesso, unique(resultados));
-cdfResultados = calcularBernoulliCDF(pmfResultados, unique(resultados));
+% Função para gerar amostras da variável aleatória customizada
+function samples = generate_custom_samples(probability, num_samples)
+  samples = rand(num_samples, 1) >= (1 - probability);
+end
 
-% Gera variáveis aleatórias Bernoulli
-numEnsaios = input('Digite o número de ensaios: ');
-amostrasAleatorias = gerarVariaveisBernoulli(probabilidadeSucesso, numEnsaios);
+% Entrada de probabilidade
+probability = input('Digite a probabilidade p (valores entre 0 e 1): ');
 
-% Plotagem:
+% Leitura do arquivo 'entrada.txt'
+file_path = 'entrada.txt';  % Modifique o caminho conforme necessário
+fid = fopen(file_path, 'r');
+
+if fid == -1
+    error(['Não foi possível abrir o arquivo: ' file_path]);
+end
+
+values = fscanf(fid, '%f');
+fclose(fid);
+
+% Cálculos
+unique_values = unique(values);
+pmf = custom_pmf(probability, unique_values);
+cdf = custom_cdf(pmf, unique_values);
+
+% Plotagem
 figure;
 
 % Subplotagem para a PMF
-subplot(2, 1, 1);
-% Gráfico de barras da PMF
-bar(unique(resultados), pmfResultados, 'b', 'LineWidth', 1.5);
-title('Distribuição Bernoulli - Função de Massa de Probabilidade (PMF)');
-xlabel('Resultado');
+subplot(2,1,1);
+bar(unique_values, pmf, 'FaceColor', 'b', 'EdgeColor', 'b');
+title('Função de Massa de Probabilidade Customizada');
+xlabel('X');
 ylabel('Probabilidade');
 ylim([0, 1]);
 grid on;
 
 % Subplotagem para a CDF
-subplot(2, 1, 2);
-% Gráfico de degrau (stairs) para a CDF
-stairs(unique(resultados), cdfResultados, 'r', 'LineWidth', 1.5);
-title('Distribuição Bernoulli - Função de Distribuição Cumulativa (CDF)');
-xlabel('Resultado');
-ylabel('Probabilidade acumulada');
-ylim([0, 1]);
+subplot(2,1,2);
+stairs(unique_values, cdf, 'LineWidth', 2, 'Color', 'r');
+title('Função de Distribuição Cumulativa Customizada');
+xlabel('X');
+ylabel('Probabilidade Acumulada');
+ylim([0, 1.1]);
 grid on;
 
-% Adiciona uma legenda para a CDF
-legend('CDF');
+% Adiciona um título geral
+annotation('textbox', [0 0.9 1 0.1], 'String', ['Análise da Distribuição Customizada (p = ' num2str(probability) ')'], 'EdgeColor', 'none', 'HorizontalAlignment', 'center');
 
